0fe90df046a9345bbbb6edebdd2b6a49dfb197d5
Kooreis <kooreis.official@gmail.com>
2022-02-08 16:00:00 -0600

    Created a public class named 'PrimeNumberFinder' and defined the main method. Initialized the Scanner object and prompted the user to enter a number.

--- Prime_Number_Finder.java
+++ Prime_Number_Finder.java
@@ -1,1 +1,6 @@
-import java.util.Scanner;
+public class PrimeNumberFinder {
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+System.out.println("Enter a number: ");
+int num = scanner.nextInt();
+}
